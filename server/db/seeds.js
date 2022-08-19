d8f78d21aa045b8fef537bf51315432c60fec2e8
Jacob <jacob_greenhalgh@hotmail.com>
2022-08-19 13:40:58 +0100

    added dependencies

---
+++ server/db/seeds.js
@@ -0,0 +1,46 @@
+use website;
+db.dropDatabase();
+
+db.categories.insertMany(
+// 	OBJECT	{ 
+	
+// 			[ 
+// 				CATEGORY1{
+// 						[ topic1{
+// 								topic.title
+// 								topic.content
+// 								topic.img
+// 							},
+// 							topic2{
+// 								topic.title
+// 								topic.content
+// 								topic.img
+// 							},
+// 						]
+// 				},
+// 					CATEGORY2{
+// 						[ topic1{
+// 								topic.title
+// 								topic.content
+// 								topic.img
+// 							},
+// 							quiz{
+// 								questions[
+// 								question1{
+// 								question.title
+// 								question.choices[
+// 									choice 1, 
+// 									choice 2,
+// 									choice 3,
+// 									choice 4]
+// 								question.correctAwnser{choice 4} 
+// 								}]	}
+// 								,
+// 						]
+// 				},
+	
+			
+// 			]
+
+// }
+)
