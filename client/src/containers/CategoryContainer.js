d7990e52bb754316547ef6ea437ed12028f08cf8
Andrew <andrewfairley95@gmail.com>
2022-08-20 13:28:53 +0100

    set up basic container for category

--- client/src/containers/CategoryContainer.js
+++ client/src/containers/CategoryContainer.js
@@ -2,23 +2,24 @@
 import { getCategories } from '../WebsiteService';
 import {useState, useEffect}  from 'react';
 
+
+// constructs Category Container 
 function CategoryContainer() {
 
-    const [message, setMessage] = useState("")
+    const [categories, setCategories] = useState([])
 
-
+// on page load categories will be fetched and set as out state
     useEffect(()=>{
         getCategories().then((result)=>{
-          setMessage(result);
-          
+          setCategories(result);
         })
       }, []);
 
 
-
+// passing down all the fetched categories as a prop to categories list
       return (
         <>
-        <p>{message[0].message}</p>
+          <CategoryList categories={categories}></CategoryList>
         </>
       )
 }
