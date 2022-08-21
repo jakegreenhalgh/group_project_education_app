9504d9e8f521898ee43ab7aae6e262c4e62a7ddf
CarlyCoding <carly_off@outlook.com>
2022-08-21 14:01:08 +0100

    basic topic card

---
+++ client/src/components/topic_components/TopicCard.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+// Import NavBar here for use, same Nav throughout
+
+const TopicCard = ({ topic }) =>{
+    // Didn't use any states as I couldn't see we would require any, as we are displaying only. 
+
+    return (
+        //Nav div here
+        <div className='Topic-Header-Image'>
+            <{topic.image}/>
+        </div>
+
+    )
+};
+
+
+// Show topic image
+
+// Show topic text 
+
+// Show small topic image (keep this same initially.)
+
+
+
