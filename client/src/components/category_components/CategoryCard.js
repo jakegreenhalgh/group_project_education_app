3eb1b17dbd05735a9ea005f3eed780983f8b22a0
CarlyCoding <carly_off@outlook.com>
2022-08-21 16:25:39 +0100

    started on Cat Card

---
+++ client/src/components/category_components/CategoryCard.js
@@ -0,0 +1,29 @@
+import {useState, useEffect } from 'react';
+// Import the nav bar 
+// Topic import/ route required? 
+
+const CategoryCard = ({ category }) => {
+    // require a state here for topic click? 
+
+    return (
+        <>
+        <div className='Nav-bar'>
+        // Code to return nav bar here
+        </div>
+        </>
+        
+    
+    )
+
+
+    }
+
+
+
+
+
+// Large header image of the chosen category
+// Display of images from category, with a title that has on click function to take to topic. 
+
+// 
+// return everything in the display
