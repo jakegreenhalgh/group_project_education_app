5b5597602e6fc4220b0cdf59d82a3d5b1ec45a3f
Andrew <andrewfairley95@gmail.com>
2022-08-22 10:25:00 +0100

    fixed bug with export and conflict in create router

--- client/src/components/category_components/CategoryCard.js
+++ client/src/components/category_components/CategoryCard.js
@@ -4,28 +4,29 @@ import {useState, useEffect } from 'react';
 
 const CategoryCard = ({ category }) => {
 
-const topics = categories.topics.map(topic => {
-    return <Topic
-        key= {topic._id}
-        topic= {topic}
-    />
-});
+// const topics = categories.topics.map(topic => {
+//     return <Topic
+//         key= {topic._id}
+//         topic= {topic}
+//     />
+// });
 
-
+// Would like to like topic images to topic cards here- new component name?- Discuss with group */
     return (
         <>
         <div className='Nav-bar'>
+            <p>{category.category}</p>
         // Code to return nav bar here
         </div>
         <div className='Category-Header-Image'>
         <img src={category.header_image}/>
         </div>
         <div className = 'Topic-menu'>
-           {topics}
-           {/* Would like to like topic images to topic cards here- new component name?- Discuss with group */}
         </div>
         </>
     
     )
 
     };
+
+    export default CategoryCard;
