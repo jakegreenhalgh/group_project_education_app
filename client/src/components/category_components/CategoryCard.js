f6b82dc035b7e9ce905e8f42e2491994f2aac639
Andrew <andrewfairley95@gmail.com>
2022-08-22 11:22:05 +0100

    can display categories and link to topics

--- client/src/components/category_components/CategoryCard.js
+++ client/src/components/category_components/CategoryCard.js
@@ -1,27 +1,21 @@
-import {useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 // Import the nav bar 
 // Topic import/ route required? 
 
 const CategoryCard = ({ category }) => {
 
-// const topics = categories.topics.map(topic => {
-//     return <Topic
-//         key= {topic._id}
-//         topic= {topic}
-//     />
-// });
+    let url = "/categories"
+    let name = category.category
 
 // Would like to like topic images to topic cards here- new component name?- Discuss with group */
     return (
         <>
         <div className='Nav-bar'>
-            <p>{category.category}</p>
-        // Code to return nav bar here
+            <Link to={`${url}/${name}`}> <h2>{category.category}</h2></Link>
         </div>
         <div className='Category-Header-Image'>
-        <img src={category.header_image}/>
-        </div>
-        <div className = 'Topic-menu'>
+        <img src={category.image}/>
+        <p>{category.description}</p>
         </div>
         </>
     
