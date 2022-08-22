942e8d4545509cef359f54cbe0190e9f6335bfc7
Andrew <andrewfairley95@gmail.com>
2022-08-22 12:56:43 +0100

    categories not displays a list of categories and allows you to navigate to a category

--- client/src/components/category_components/CategoryCard.js
+++ client/src/components/category_components/CategoryCard.js
@@ -1,23 +1,21 @@
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import TopicContainer from "../../containers/TopicContainer";
 // Import the nav bar 
 // Topic import/ route required? 
 
 const CategoryCard = ({ category }) => {
 
-    let url = "/categories"
-    let name = category.category
-
 // Would like to like topic images to topic cards here- new component name?- Discuss with group */
     return (
-        <>
+    <>
         <div className='Nav-bar'>
-            <Link to={`${url}/${name}`}> <h2>{category.category}</h2></Link>
+            <Link to={`/categories/${category._id}`}> <h2>{category.category}</h2></Link>
         </div>
         <div className='Category-Header-Image'>
         <img src={category.image}/>
         <p>{category.description}</p>
         </div>
-        </>
+    </>
     
     )
 
