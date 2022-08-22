a59c1ba6af0db09abba5bb7e9dcca06cc45a6d7d
Jack Knowles <jackmhknowles@gmail.com>
2022-08-22 13:56:54 +0100

    updated TopiceContainer

--- client/src/containers/TopicContainer.js
+++ client/src/containers/TopicContainer.js
@@ -1,15 +1,27 @@
 // Components - TopicList, TopicCard
 
 import React, { useEffect, useState } from "react";
+import { Link, Route, Routes, useParams,} from "react-router-dom";
 import TopicList from "../components/topic_components/TopicList";
+import { showCategory } from "../WebsiteService";
 
-const TopicContainer = (topics) => {
+const TopicContainer = () => {
+	const [topics, setTopics] = useState([]);
 
-	const [topics, setTopics] = useState();	
+	const {id} = useParams();
+
+
+    useEffect(()=>{
+        showCategory(id).then((result)=>{
+          setTopics(result.content);
+        })
+      }, []);
+
 
 	return (
 		<>
 			<TopicList topics={topics}/>
+			<p>Work pls ty</p>
 		</>
 	);
 };
