457d34cd12d650703a4caf3eaa0902413ffeed03
CarlyCoding <carly_off@outlook.com>
2022-08-23 10:31:47 +0100

    endangered species completed

--- server/db/our_world_seeds.js
+++ server/db/our_world_seeds.js
@@ -5,11 +5,11 @@ db.categories.insertMany(
         [
             {   "category": "Endangered Species",
                 "image": "https://unsplash.com/photos/Uu1CtKngEXY",
-                "Description": "An endangered species is a species that is very likely to become extinct in the near future, either worldwide or in a particular political jurisdiction. Endangered species may be at risk due to factors such as habitat loss, poaching and invasive species."
+                "description": "An endangered species is a species that is very likely to become extinct in the near future, either worldwide or in a particular political jurisdiction. Endangered species may be at risk due to factors such as habitat loss, poaching and invasive species."
                 [
                     {
                         "_id": ObjectId(),
-                        "type": XXXXX ,
+                        "type": "facts" ,
                         "name": "The Snow Leopard",
                         "url": "https://www.wwf.org.uk/learn/wildlife/snow-leopards",
                         "image": "https://unsplash.com/photos/HFIvhaOcHVA",
@@ -17,23 +17,46 @@ db.categories.insertMany(
                     },
                     {
                         "_id": ObjectId(),
-                        "type": XXXXX ,
+                        "type": "facts" ,
                         "name": "The Sunda Island Tiger",
                         "url": "https://www.worldwildlife.org/species/sunda-tiger",
                         "image":"https://unsplash.com/photos/-KNNQqX9rqY",
                         "text": "This isn't a picture of the Sunda Island Tiger, it's just a normal one. The Sunda Island tiger is so endangered there are only 400 left in the world (estimated)",
-
                     }
                 ]
             },
 
             {   "category": "Rising Sea Levels",
-                "image":
-                "description"
+                // "image": INSERT
+                "description": "As the title suggests the sea levels are rising. They are rising at double the rate they were in 1900. Did you know by 2050 lots of England will be underwater? Did you know that's only 28 years away? Why is no one doing anything about this?"
+                [
+                    {
+                        "_id": ObjectId(),
+                        "type":
+                        "name": 
+                        "url":
+                        "image":
+                        "text":,
+
+                    },
+                    {
+
+                    }
+                ]
             
             },
 
             {   "category": "Deforestation",
+                // "Image": INSERT
+                "description":
+                [
+                    {
+
+                    },
+                    {
+
+                    }
+                ]
 
             }
 
