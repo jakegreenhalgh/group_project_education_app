bcbde9b396eba5e7f7f689fef1725791150ead0e
CarlyCoding <carly_off@outlook.com>
2022-08-23 10:41:42 +0100

    skeleton for all contents laid out

--- server/db/our_world_seeds.js
+++ server/db/our_world_seeds.js
@@ -5,8 +5,8 @@ db.categories.insertMany(
         [
             {   "category": "Endangered Species",
                 "image": "https://unsplash.com/photos/Uu1CtKngEXY",
-                "description": "An endangered species is a species that is very likely to become extinct in the near future, either worldwide or in a particular political jurisdiction. Endangered species may be at risk due to factors such as habitat loss, poaching and invasive species."
-                [
+                "description": "An endangered species is a species that is very likely to become extinct in the near future, either worldwide or in a particular political jurisdiction. Endangered species may be at risk due to factors such as habitat loss, poaching and invasive species.",
+                "content": [
                     {
                         "_id": ObjectId(),
                         "type": "facts" ,
@@ -28,19 +28,24 @@ db.categories.insertMany(
 
             {   "category": "Rising Sea Levels",
                 // "image": INSERT
-                "description": "As the title suggests the sea levels are rising. They are rising at double the rate they were in 1900. Did you know by 2050 lots of England will be underwater? Did you know that's only 28 years away? Why is no one doing anything about this?"
-                [
+                "description": "As the title suggests the sea levels are rising. They are rising at double the rate they were in 1900. Did you know by 2050 lots of England (and plenty of Scotland) will be underwater? Did you know that's only 28 years away? Why is no one doing anything about this?",
+                "content": [
                     {
                         "_id": ObjectId(),
-                        "type":
-                        "name": 
+                        "type": "facts",
+                        "name": "Facts about the rising sea",
                         "url":
                         "image":
                         "text":,
 
                     },
                     {
-
+                        "_id": ObjectId(),
+                        "type": "facts",
+                        "name": "Skara Brae and the rising sea levels",
+                        "url":
+                        "image":
+                        "text":,
                     }
                 ]
             
@@ -48,13 +53,24 @@ db.categories.insertMany(
 
             {   "category": "Deforestation",
                 // "Image": INSERT
-                "description":
-                [
+                "description":   ,
+                "content": [
                     {
+                        "_id": ObjectId(),
+                        "type": "facts",    
+                        "name": "Why deforestation is really bad in general"
+                        "url":
+                        "image":
+                        "text":,
 
                     },
                     {
-
+                        "_id": ObjectId(),
+                        "type": "facts",
+                        "name": "Deforestation adn the Amazon Rainforest "
+                        "url":
+                        "image":
+                        "text":,
                     }
                 ]
 
