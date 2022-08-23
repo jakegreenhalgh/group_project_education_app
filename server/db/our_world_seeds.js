977e1f9899278621d18d9cd914aec926dca63166
CarlyCoding <carly_off@outlook.com>
2022-08-23 11:07:29 +0100

    Deforestation data 70% completed

--- server/db/our_world_seeds.js
+++ server/db/our_world_seeds.js
@@ -4,7 +4,7 @@ db.dropDatabase();
 db.categories.insertMany(
         [
             {   "category": "Endangered Species",
-                "image": "https://unsplash.com/photos/Uu1CtKngEXY",
+                // "image": INSERT",
                 "description": "An endangered species is a species that is very likely to become extinct in the near future, either worldwide or in a particular political jurisdiction. Endangered species may be at risk due to factors such as habitat loss, poaching and invasive species.",
                 "content": [
                     {
@@ -12,7 +12,7 @@ db.categories.insertMany(
                         "type": "facts" ,
                         "name": "The Snow Leopard",
                         "url": "https://www.wwf.org.uk/learn/wildlife/snow-leopards",
-                        "image": "https://unsplash.com/photos/HFIvhaOcHVA",
+                        // "image": INSERT,
                         "text": "Snow leopards are so endangered that it's incredibly difficult to find free images of them on unsplash",
                     },
                     {
@@ -20,7 +20,7 @@ db.categories.insertMany(
                         "type": "facts" ,
                         "name": "The Sunda Island Tiger",
                         "url": "https://www.worldwildlife.org/species/sunda-tiger",
-                        "image":"https://unsplash.com/photos/-KNNQqX9rqY",
+                        // "image":INSERT,
                         "text": "This isn't a picture of the Sunda Island Tiger, it's just a normal one. The Sunda Island tiger is so endangered there are only 400 left in the world (estimated)",
                     }
                 ]
@@ -53,21 +53,21 @@ db.categories.insertMany(
 
             {   "category": "Deforestation",
                 // "Image": INSERT
-                "description":   ,
+                "description": "Deforestation is occurring at an incredible speed, by the time you say the word 'deforestation' another chunk of forest the size of a football pitch has been destroyed. Forests provide habitats to endangered species, affect the rainfall cycle, and turn Co2 into oxygen",
                 "content": [
                     {
                         "_id": ObjectId(),
                         "type": "facts",    
-                        "name": "Why deforestation is really bad in general"
-                        "url":
+                        "name": "Why deforestation is really bad in general",
+                        "url":"https://www.wwf.org.uk/learn/effects-of/deforestation",
                         // "image": INSERT 
-                        "text":,
+                        "text":"Forest loss and damage is the cause of around 10% of global warming. There is simply no way we can fight the climate crisis if we do not stop deforestation.",
 
                     },
                     {
                         "_id": ObjectId(),
                         "type": "facts",
-                        "name": "Deforestation adn the Amazon Rainforest "
+                        "name": "Deforestation and the Amazon Rainforest",
                         "url":
                         // "image": INSERT
                         "text":,
