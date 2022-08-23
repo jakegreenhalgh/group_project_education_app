dc9667c2a6eff8c273f78620ff86e825e4434c76
CarlyCoding <carly_off@outlook.com>
2022-08-23 21:08:02 +0100

    divved up the contentcard for CSS

--- client/src/components/Content_Components/ContentCard.js
+++ client/src/components/Content_Components/ContentCard.js
@@ -1,15 +1,24 @@
+import './content.css';
+
 const ContentCard = ({ content }) => {
 
     // Would like to like topic images to topic cards here- new component name?- Discuss with group */
         return (
         <>
-            <div className='Nav-bar'>
-            <img src={content.images[0].header}/>    
-            <h2>{content.name}</h2>
-            <p>{content.text}</p>
-            <img src={content.images[1].body1}/>  
+            <div className='header-image'>
+            <img src={content.images[0].header}/>
             </div>
-            <div className='Category-Header-Image'>
+
+            <div className='article-header'> 
+            <h2>{content.name}</h2>
+            </div>
+            
+            <div className='article-body'>
+            <p>{content.text}</p>
+            </div>
+
+            <div className='article-image'>
+            <img src={content.images[1].body1}/> 
             </div>
         </>
         
