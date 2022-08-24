5dd4e72f64fae8a4bdc318655de70a7281200a4d
CarlyCoding <carly_off@outlook.com>
2022-08-24 11:57:52 +0100

    added new seeds for improved topic card page

--- server/db/added_seeds.js
+++ server/db/added_seeds.js
@@ -2,53 +2,49 @@
 
 // // Biodiversity
 
+{
+"_id": ObjectId(),                                            
+"type": "article",
+"name": "Coral Reefs and their Biodiversity",
+"url": "https://coral.org/en/coral-reefs-101/",
+"images": [
+            {"header": "https://images.unsplash.com/photo-1543181831-dadb192c045f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"},
+            {"body1": "https://images.unsplash.com/photo-1613117908785-381bafe45361?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2364&q=80"} 
+            ],
+"text1":"Coral reefs provide an important ecosystem for life underwater.they protect coastal areas by reducing the power of waves hitting the coast,and provide a crucial source of income for millions of people.",
+"text2":"Coral reefs teem with diverse life. Thousands of species can be found living on one reef.The Great Barrier Reef contains over 400 coral species, 1,500 fish species, 4,000 mollusc species and six of the world's seven sea turtle species.
+"text3":"The Coral Triangle - a coral-rich marine region in Southeast Asia that encompasses the waters between Indonesia, Malaysia, the Philippines and Papua New Guinea - is the most biologically diverse marine ecosystem on Earth."	
 
-// "_id": ObjectId(),                                            
-// "type": "article",
-// "name": "Coral Reefs and their Biodiversity",
-// "url": "https://coral.org/en/coral-reefs-101/",
-// "images": [
-//             {"header": "https://images.unsplash.com/photo-1543181831-dadb192c045f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"},
-//             {"body1": "https://images.unsplash.com/photo-1613117908785-381bafe45361?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2364&q=80"} 
-//             ],
-// "text": 
-// "Coral reefs provide an important ecosystem for life underwater
-// protect coastal areas by reducing the power of waves hitting the coast,
-// and provide a crucial source of income for millions of people.
-
-// Coral reefs teem with diverse life. Thousands of species can be found living on one reef. 
-// The Great Barrier Reef contains over 400 coral species, 1,500 fish species, 4,000 mollusc species and six of the world's 
-// seven sea turtle species. The Coral Triangle - a coral-rich marine region in Southeast Asia that encompasses the waters 
-// between Indonesia, Malaysia, the Philippines and Papua New Guinea - is the most biologically diverse marine ecosystem on 
-// Earth."						
-// },
+},
 
 
-// {
-// "_id": ObjectId(),
-// "type": "article",
-// "name": "Scotlands wildflower meadows",
-// "url": "https://scottishwildlifetrust.org.uk/our-work/our-projects/save-our-magnificent-meadows/",
-// "images": [
-//             {"header": "https://images.unsplash.com/photo-1483141531895-191b34d33fd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"},
-//             {"body1": "https://images.unsplash.com/photo-1505990482843-ba05d52c02c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"} 
-//             ],
-// "text": "Instead of plain cut grass on verges and public places, Historic Environment Scotland and many other organisations have made the descision to plant wildflowers on grassland. These wildflowers provide a space for bees to pollinate and as a result is excellent for the environment."		
-// }
+{
+"_id": ObjectId(),
+"type": "article",
+"name": "Scotlands wildflower meadows",
+"url": "https://education.nationalgeographic.org/resource/tidal-energy",
+"images": [
+            {"header": "https://images.unsplash.com/photo-1560260240-c6ef90a163a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGlkYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
+            {"body1": "https://images.unsplash.com/photo-1505990482843-ba05d52c02c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"} 
+            ],
+"text": "Instead of plain cut grass on verges and public places, Historic Environment Scotland and many other organisations have made the descision to plant wildflowers on grassland. 
+"text2": "These wildflowers provide a space for bees to pollinate and as a result is excellent for the environment."		
+}
 
 
 
-// // Renewables
+// Renewables
 
-// {
-//     "_id": ObjectId(),
-//     "type": "article",
-//     "name": "Wind Farms Are Great",
-//     "url": "https://www.wwf.org.uk/updates/increased-offshore-wind-ambition-2030",
-//     "images": [
-//                 {"header": "https://images.unsplash.com/photo-1544250726-cf5d75ffc212?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1515&q=80"},
-//                 {"body1": "https://images.unsplash.com/photo-1587894736501-4e0131bf60b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1464&q=80"} 
-//                 ],
-//     "text": "INCREASED OFFSHORE WIND AMBITION BY 2030 – WWF Scotland comment - Commenting on the release today of the Scottish Government’s new ambition to increase offshore wind capacity [1], Fabrice Leveque, head of policy at WWF Scotland said: ‘Offshore wind is already powering hundreds of thousands of Scottish homes and this new plan will help grow an industry that’s vital to reach net-zero climate emissions by 2045. Each project creates hundreds of jobs and can help put Scotland on a path to a green recovery. In order to ensure local communities and businesses benefit, it’s imperative the UK and Scottish Governments work closely together to ensure as much work as possible goes to local supply chains’"						
-// },
+{
+    "_id": ObjectId(),
+    "type": "article",
+    "name": "Wave energy is renewable",
+    "url": "https://www.wwf.org.uk/updates/increased-offshore-wind-ambition-2030",
+    "images": [
+                {"header": "https://images.unsplash.com/photo-1544250726-cf5d75ffc212?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1515&q=80"},
+                {"body1": "https://images.unsplash.com/photo-1514911215070-20b41057e8ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d2F2ZSUyMHBvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"} 
+                ],
+    "text": "Tidal energy is power produced by the surge of ocean waters during the rise and fall of tides."
+    "text2": "Tidal energy is a renewable source of energy." "During the 20th century, engineers developed ways to use tidal movement to generate electricity in areas where there is a significant tidal range—the difference in area etween high tide and low tide. All methods use special generators to convert tidal energy into electricity."						
+},
 
--- server/db/seeds.js
+++ server/db/seeds.js
@@ -28,7 +28,35 @@ db.categories.insertMany(
                                                         {"body1": "https://images.unsplash.com/photo-1587894736501-4e0131bf60b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1464&q=80"} 
                                                         ],
                                             "text": "Example Content Number Two. Example Content Number Two.  Example Content Number Two. Example Content Number Two. Example Content Number Two. Example Content Number Two."		
-                                        }		
+                                        },
+
+                                        {
+                                            "_id": ObjectId(),
+                                            "type": "article",
+                                            "name": "Coral Reefs and their Biodiversity",
+                                            "url": "https://coral.org/en/coral-reefs-101/",
+                                            "images": [
+                                                    {"header": "https://images.unsplash.com/photo-1543181831-dadb192c045f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"},
+                                                    {"body1": "https://images.unsplash.com/photo-1613117908785-381bafe45361?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2364&q=80"} 
+                                                    ],
+                                            "text1":"Coral reefs provide an important ecosystem for life underwater.they protect coastal areas by reducing the power of waves hitting the coast,and provide a crucial source of income for millions of people.",
+                                            "text2":"Coral reefs teem with diverse life. Thousands of species can be found living on one reef.The Great Barrier Reef contains over 400 coral species, 1,500 fish species, 4,000 mollusc species and six of the world's seven sea turtle species",
+                                            "text3":"The Coral Triangle - a coral-rich marine region in Southeast Asia that encompasses the waters between Indonesia, Malaysia, the Philippines and Papua New Guinea - is the most biologically diverse marine ecosystem on Earth."	
+                                            
+                                        },	
+
+                                        {
+                                            "_id": ObjectId(),
+                                            "type": "article",
+                                            "name": "Example Article Number Two",
+                                            "url": "https://www.google.co.uk/",
+                                            "images": [
+                                                {"header": "https://images.unsplash.com/photo-1560260240-c6ef90a163a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dGlkYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
+                                                {"body1": "https://images.unsplash.com/photo-1505990482843-ba05d52c02c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"} 
+                                                ],
+                                                "text": "Instead of plain cut grass on verges and public places, Historic Environment Scotland and many other organisations have made the descision to plant wildflowers on grassland.",
+                                                "text2": "These wildflowers provide a space for bees to pollinate and as a result is excellent for the environment."
+                                        }	
                                 ]
                 },
                 {   "category": "renewables",
@@ -56,6 +84,18 @@ db.categories.insertMany(
                                                     {"body1": "https://images.unsplash.com/photo-1587894736501-4e0131bf60b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1464&q=80"} 
                                                     ],
                                         "text": "Example Content Number Three. Example Content Number Three.  Example Content Number Three. Example Content Number Three. Example Content Number Three. Example Content Number Three."		
+                                    },
+                                    {
+                                        "_id": ObjectId(),
+                                        "type": "article",
+                                        "name": "Wave energy is renewable",
+                                        "url": "https://www.wwf.org.uk/updates/increased-offshore-wind-ambition-2030",
+                                        "images": [
+                                                    {"header": "https://images.unsplash.com/photo-1544250726-cf5d75ffc212?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1515&q=80"},
+                                                    {"body1": "https://images.unsplash.com/photo-1514911215070-20b41057e8ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d2F2ZSUyMHBvd2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"} 
+                                                    ],
+                                        "text": "Tidal energy is power produced by the surge of ocean waters during the rise and fall of tides.",
+                                        "text2": "Tidal energy is a renewable source of energy. During the 20th century, engineers developed ways to use tidal movement to generate electricity in areas where there is a significant tidal range—the difference in area etween high tide and low tide. All methods use special generators to convert tidal energy into electricity."					
                                     },
                                     {
                                         "_id": ObjectId(),
